4dbf31b4fc30d40ce510334a96371646fd03dd3d
Samuel Doblado <samuel.dobladoalmeida@gmail.com>
2018-02-06 19:15:27 +0000

    9th

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,11 @@ Rails.application.routes.draw do
   
   resources :sessions, only: [:new, :create, :destroy]
   get "/login" => "sessions#new", as: "login"
-  delete "/logout" => "sessions#destroy", as: "logout"
-  get "/allbooks" => "allbooks#index"
-  get "/allbooks/:id" => "allbooks#show"
-  get "/new_reviewer" => "reviewers#new"
-  get "/reviewers" => "reviewers#index"
-  get "/edit_reviewer" => "reviewers#edit"
-  post "/reviewers/" => "reviewers#create"
+  #delete "/logout" => "sessions#destroy", as: "logout"
+  #get "/allbooks" => "allbooks#index"
+  #get "/allbooks/:id" => "allbooks#show"
+  #get "/new_reviewer" => "reviewers#new"
+  #get "/reviewers" => "reviewers#index"
+  #get "/edit_reviewer" => "reviewers#edit"
+  #post "/reviewers/" => "reviewers#create"
 end
