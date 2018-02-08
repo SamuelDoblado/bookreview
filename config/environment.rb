aed24e8a2da1e7e7861042878a890ca7e67367d8
Samuel Doblado <samuel.dobladoalmeida@gmail.com>
2018-02-08 16:31:32 +0000

    App con CSS Y reCaptcha

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ENV['RECAPTCHA_PUBLIC_KEY']  = '6LcgQUUUAAAAABpG33pj_uqyAD2FUvrmypuRqH-o'
+ENV['RECAPTCHA_PRIVATE_KEY'] = '6LcgQUUUAAAAAImXx8jbz-Jvl2u8UIlr0D-4qMu5'
